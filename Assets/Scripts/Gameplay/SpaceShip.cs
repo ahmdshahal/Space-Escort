b05241c1114025df1aa80bd72622e45bc8eeedcb
ahmdshahal <ahmdshahal29@gmail.com>
2023-12-09 09:27:23 +0700

    Add Explosion Space Ship

--- Assets/Scripts/Gameplay/SpaceShip.cs
+++ Assets/Scripts/Gameplay/SpaceShip.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class SpaceShip : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem explosionEffect;
+    [SerializeField] private MeshRenderer shipRenderer;
     [SerializeField] private Slider healthBar;
     [SerializeField] private float health;
 
@@ -23,6 +25,7 @@ public class SpaceShip : MonoBehaviour
 
         if (_currentHealth <= 0)
         {
+            StartCoroutine(Dead());
             //GameOver
         }
     }
@@ -30,5 +33,17 @@ public class SpaceShip : MonoBehaviour
     private void SetUIHealth()
     {
         healthBar.value = _currentHealth / health;
+    }
+    
+    private IEnumerator Dead()
+    {
+        explosionEffect.Play();
+
+        shipRenderer.enabled = false;
+        
+        ParticleSystem.MainModule mainModule = explosionEffect.main;
+        yield return new WaitForSeconds(mainModule.duration);
+        
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/General/MenuController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void GoToScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
